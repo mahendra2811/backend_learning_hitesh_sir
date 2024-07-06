a5ce02c7007373a28b614e990ece361b8c3b6d9c
Mahendra Singh Puniya <mahendrapuniya92@gmail.com>
2024-07-06 14:22:47 +0530

    modified

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-const registerUser = aysyncHandler (async(req, res) =>{
+const registerUser = asyncHandler (async(req, res) =>{
     res.status(200).json({
         message:"OK"
     })
